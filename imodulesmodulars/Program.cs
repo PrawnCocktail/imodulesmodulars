d5ed368aba5d9335ea0abe365ebe7139b2ad17cc
PrawnCocktail <5621958+PrawnCocktail@users.noreply.github.com>
2021-03-23 23:14:28 +0000

    Add project files.

---
+++ imodulesmodulars/Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imodulesmodulars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0";
+                var channelInfoJson = client.DownloadString("https://api.gotostage.com/channels/imodulesmodulars");
+                var channelInfo = JsonConvert.DeserializeObject<ChannelInfo>(channelInfoJson);
+
+                var contentKeysJson = client.DownloadString("https://api.gotostage.com/channels/" + channelInfo.channelKey + "/groups?keys=" + channelInfo.defaultGroupKey);
+                var contentKeyInfo = JsonConvert.DeserializeObject<List<ContentList>> (contentKeysJson);
+
+                foreach (var video in contentKeyInfo[0].contentKeys)
+                {
+                    try
+                    {
+                        var videoInfo = client.DownloadString("https://api.gotostage.com/v2/contents/" + video + "/assets");
+                        var videoResources = JsonConvert.DeserializeObject<VideoInfo>(videoInfo);
+
+                        var videotitlejson = client.DownloadString("https://api.gotostage.com/contents?ids=" + video);
+                        var videoTitle = JsonConvert.DeserializeObject<List<VideoTitle>>(videotitlejson);
+
+                        foreach (var resource in videoResources.resources)
+                        {
+                            if (resource.resourceType == "recording")
+                            {
+                                foreach (var type in resource.attributes)
+                                {
+                                    if (type.name == "cdnlocation")
+                                    {
+                                        if (!File.Exists(videoTitle[0].title + ".mp4"))
+                                        {
+                                            Console.WriteLine("Downloading Video: " + videoTitle[0].title);
+                                            client.DownloadFile(type.value, videoTitle[0].title + ".mp4");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine(videoTitle[0].title + " video already exists, Skipping.");
+                                        }
+                                    }
+                                }
+                            }
+                            if (resource.resourceType == "parsed_transcript")
+                            {
+                                foreach (var type in resource.attributes)
+                                {
+                                    if (type.name == "cdnlocation")
+                                    {
+                                        if (!File.Exists(videoTitle[0].title + "_transcript.json"))
+                                        {
+                                            Console.WriteLine("Downloading Transcript: " + videoTitle[0].title);
+                                            client.DownloadFile(type.value, videoTitle[0].title + "_transcript.json");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine(videoTitle[0].title + " transcript already exists, Skipping.");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ imodulesmodulars/json.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imodulesmodulars
+{
+    public class ChannelInfo
+    {
+        public string channelKey { get; set; }
+        public string channelTitle { get; set; }
+        public List<object> groupKeys { get; set; }
+        public string defaultGroupKey { get; set; }
+        public bool isLimitedAudience { get; set; }
+        public List<string> aliases { get; set; }
+        public DateTime createTime { get; set; }
+        public DateTime updateTime { get; set; }
+        public string channelDescription { get; set; }
+    }
+
+    public class ContentList
+    {
+        public string groupKey { get; set; }
+        public string groupName { get; set; }
+        public string channelKey { get; set; }
+        public List<string> contentKeys { get; set; }
+        public DateTime createTime { get; set; }
+        public DateTime updateTime { get; set; }
+    }
+
+    public class Attribute
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Resource
+    {
+        public string resourceKey { get; set; }
+        public string status { get; set; }
+        public string statusId { get; set; }
+        public string name { get; set; }
+        public string resourceType { get; set; }
+        public string resourceTypeId { get; set; }
+        public string provider { get; set; }
+        public int version { get; set; }
+        public List<Attribute> attributes { get; set; }
+    }
+
+    public class VideoInfo
+    {
+        public string status { get; set; }
+        public List<Resource> resources { get; set; }
+    }
+
+    public class VideoTitle
+    {
+        public string title { get; set; }
+    }
+}
